3d3b688f0a46c89995206049220492010397f727
unknown <g.maziliauskas96@gmail.com>
2022-05-19 20:35:29 +0300

    Finished SpringBoot

---
+++ src/main/java/com/finaltest/NaujokuManageris/NaujokuManagerisApplication.java
@@ -0,0 +1,13 @@
+package com.finaltest.NaujokuManageris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaujokuManagerisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaujokuManagerisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finaltest/NaujokuManageris/NaujokuManagerisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finaltest.NaujokuManageris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaujokuManagerisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
